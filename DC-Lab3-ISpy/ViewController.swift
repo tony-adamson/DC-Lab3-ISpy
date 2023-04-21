bba4690878ade1a8d957ad6ed38d0ab7babf06bf
Anton <antony.adamson@gmail.com>
2023-04-21 23:00:13 +0600

    Implement the UIScrollViewDelegate

--- DC-Lab3-ISpy/ViewController.swift
+++ DC-Lab3-ISpy/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var imageView: UIImageView!
@@ -15,8 +15,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        scrollView.delegate = self
     }
 
-
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func updateForZoom(size: CGSize) {
+        let widthScale = size.width / imageView.bounds.width
+        let heightScale = size.height / imageView.bounds.height
+        let scale = min(widthScale, heightScale)
+        scrollView.minimumZoomScale = scale
+        scrollView.zoomScale = scale
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        updateForZoom(size: view.bounds.size)
+    }
 }
 
