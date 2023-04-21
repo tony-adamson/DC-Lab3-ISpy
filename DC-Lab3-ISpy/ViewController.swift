4dd735596ee590d6883a8d30ab77969885aaf763
Anton <antony.adamson@gmail.com>
2023-04-21 22:26:42 +0600

    Set Up the Storyboard

--- DC-Lab3-ISpy/ViewController.swift
+++ DC-Lab3-ISpy/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
